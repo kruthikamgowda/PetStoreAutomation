4051a4ee1c848d19cb8e1a3f00f7b922d6de4faa
kruthikamgowda <kruthikamgowda15@gmail.com>
2023-10-18 14:50:18 +0530

    this is my first commit

---
+++ src/test/java/api/payload/User.java
@@ -0,0 +1,64 @@
+package api.payload;
+
+public class User {
+	int id;
+	String username;
+	String firstName;
+	String lastName;
+	String email;
+	String passowrd;
+	String phone;
+	int userStatus=0;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassowrd() {
+		return passowrd;
+	}
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getUserStatus() {
+		return userStatus;
+	}
+	public void setUserStatus(int userStatus) {
+		this.userStatus = userStatus;
+	}
+	
+	
+	
+}
